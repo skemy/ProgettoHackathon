74f69c8e4c0c64a5c1f6b1edb7b72e79f5b8e3d5
Giuseppe Iazzetta <giuseppeiazzetta05@gmail.com>
2026-03-23 11:42:44 +0100

    java doc readme

---
+++ documentazione/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"dao"},{"l":"database"},{"l":"exceptions"},{"l":"gui"},{"l":"model"},{"l":"utils"}];updateSearchResults();
